509bba056ee3143381b76ae605bd69c0b9345d85
Seann-Moser <seannmoser@gmail.com>
2024-08-01 11:47:18 -0700

    chore: add code completions

--- nvm_config/init.lua
+++ nvm_config/init.lua
@@ -1,1 +1,9 @@
 require("config.lazy")
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.o.statuscolumn = "%s %l %r"
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
--- nvm_config/lua/plugins/lsp.lua
+++ nvm_config/lua/plugins/lsp.lua
@@ -3,31 +3,41 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
         local lspconfig = require("lspconfig")
-        lspconfig.lua_ls.setup({})
-        lspconfig.bashls.setup({})
-        lspconfig.clangd.setup({})
-        -- "csharp_ls.setup({})
-        lspconfig.cmake.setup({})
-        lspconfig.cssls.setup({})
-        lspconfig.dockerls.setup({})
-        lspconfig.eslint.setup({})
-        lspconfig.flux_lsp.setup({})
-        lspconfig.gopls.setup({})
-        lspconfig.html.setup({})
-        lspconfig.jsonls.setup({})
-        -- "javatls.setup({})
-        lspconfig.ansiblels.setup({})
-        lspconfig.autotools_ls.setup({})
-        lspconfig.intelephense.setup({})
-        lspconfig.sqlls.setup({})
-        lspconfig.terraformls.setup({})
-        lspconfig.vuels.setup({})
-        lspconfig.yamlls.setup({})
+        local capabilites = require("cmp_nvim_lsp").default_capabilities()
 
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-        vim.keymap.set('n', 'gI', vim.lsp.buf.implementation, {})
-        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
+        lspconfig.lua_ls.setup({
+            capabilites = capabilites,
+        })
+        lspconfig.bashls.setup({
+            capabilites = capabilites,
+        })
+        lspconfig.clangd.setup({
+            capabilites = capabilites,
+        })
+        -- "csharp_ls.setup({})
+        lspconfig.cmake.setup({
+            capabilites = capabilites,
+        })
+        lspconfig.cssls.setup({ capabilites = capabilites, })
+        lspconfig.dockerls.setup({ capabilites = capabilites, })
+        lspconfig.eslint.setup({ capabilites = capabilites, })
+        lspconfig.flux_lsp.setup({ capabilites = capabilites, })
+        lspconfig.gopls.setup({ capabilites = capabilites, })
+        lspconfig.html.setup({ capabilites = capabilites, })
+        lspconfig.jsonls.setup({ capabilites = capabilites, })
+        -- "javatls.setup({})
+        lspconfig.ansiblels.setup({ capabilites = capabilites, })
+        lspconfig.autotools_ls.setup({ capabilites = capabilites, })
+        lspconfig.intelephense.setup({ capabilites = capabilites, })
+        lspconfig.sqlls.setup({ capabilites = capabilites, })
+        lspconfig.terraformls.setup({ capabilites = capabilites, })
+        lspconfig.vuels.setup({ capabilites = capabilites, })
+        lspconfig.yamlls.setup({ capabilites = capabilites, })
+
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "gI", vim.lsp.buf.implementation, {})
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
 }
