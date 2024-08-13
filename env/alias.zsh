1d9e0262a8cf480a23b6ee1cb4c6fd359efad736
Seann-Moser <seannmoser@gmail.com>
2024-08-12 18:17:20 -0700

    fix: add update cmd

--- env/alias.zsh
+++ env/alias.zsh
@@ -13,3 +13,5 @@ alias refresh="source ~/.zshrc"
 # Docker
 alias dcud='docker compose up -d'
 alias dcub='docker compose up --build'
+
+alias update='sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && brew update && brew upgrade && flatpak update'
